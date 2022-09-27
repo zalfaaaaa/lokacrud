9a9e7bddd4330cc4753d1b16ed612e4a03d17d32
ZALFA <110007888+zalfaaaaa@users.noreply.github.com>
2022-09-27 10:01:26 +0700

    Delete delete.php

--- dbduabelas/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$nis=$_GET['nis'];
-
-$d = new PDO('mysql:host=localhost;dbname=nicole','root',''); 
-$query = $d->query("DELETE FROM `siswa` WHERE `nis`='$nis' ");
-
-if($query){
-    header('location:crud.php');}
